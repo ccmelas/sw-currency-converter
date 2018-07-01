49b79122651901dedda354dec51a5399940c1b79
Chiemela Chinedum <chiemelachinedum@gmail.com>
2018-07-01 18:09:06 +0100

    Updates sw.js to accomodate sw-currency-converter/

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ importScripts('js/cache-polyfill.js');
 const CACHE_VERSION = 'sw-currency-converter-v28';
 const CACHE_FILES = [
     '/',
+    '/sw-currency-converter/',
     'js/main.js',
     'js/converter.js',
     'css/main.css',
